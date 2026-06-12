8d0659b53223c1bd002b9df07fd8c5ff51f9a7ee
Shane Landmeier <shane_landmeier@outlook.com>
2026-06-12 10:58:08 -0700

    Deploying a cluster of web servers

--- main.tf
+++ main.tf
@@ -13,10 +13,10 @@ output "instance_public_ip" {
     value = aws_instance.example.public_ip
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0fb653ca2d3203ac1"
-    instance_type = "c7i-flex.large"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+resource "aws_launch_configuration" "example" {
+    image_id           = "ami-0fb653ca2d3203ac1"
+    instance_type      = "c7i-flex.large"
+    security_groups    = [aws_security_group.instance.id]
 
     user_data = <<-EOF
                 #!/bin/bash
@@ -25,10 +25,23 @@ resource "aws_instance" "example" {
                 nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 
-    user_data_replace_on_change = true
-    
-    tags = {
-        Name = "terraform-example"
+    # Required when using a launch configuration with an Auto Scaling group
+    lifecycle {
+        create_before_destroy = true
+    }
+} 
+
+resource "aws_autoscaling_group" "example" {
+    launch_configuration      = aws_launch_configuration.example.name
+    vpc_zone_identifier       = [data.aws_subnet.default.id]
+
+    min_size                  = 2
+    max_size                  = 10
+
+    tag {
+        key                 = "Name"
+        value               = "terraform-example-instance"
+        propagate_at_launch = true
     }
 }
 
@@ -41,5 +54,16 @@ resource "aws_security_group" "instance" {
         to_port     = var.server_port
         protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+data "aws_vpc" "default" {
+    default = true
+}
+
+data "aws_subnet" "default" {
+    filter {
+        name   = "vpc-id"
+        values = [data.aws_vpc.default.id]
     }
 }
